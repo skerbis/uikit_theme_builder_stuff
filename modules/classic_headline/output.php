e7b3e3b4855713004014fda3f3cc63223bacb644
Thomas Skerbis <thomas.skerbis@mac.com>
2025-11-18 09:49:17 +0100

    Update module classic_headline output

---
+++ modules/classic_headline/output.php
@@ -0,0 +1,10 @@
+<section class="uk-section uk-padding-remove uk-background-REX_VALUE[2]">
+<div class="uk-container uk-padding uk-container-small uk-margin-small-bottom">
+<REX_VALUE[17] class="REX_VALUE[18]REX_VALUE[19]"><span>REX_VALUE[16]</span></REX_VALUE[17]>
+<?php if ('REX_VALUE[15]'!='')
+{?>    
+<REX_VALUE[14] class="uk-margin-remove uk-padding-removeREX_VALUE[13]"><span>REX_VALUE[15]</span></REX_VALUE[14]> 
+<?php } ?>    
+</div>    
+</section>    
+    
